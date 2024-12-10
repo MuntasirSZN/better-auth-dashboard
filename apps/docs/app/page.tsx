34c401cd8c07dab0422d774564f30ce09a004dab
Multinite <founders.multinite@gmail.com>
2024-12-10 14:46:48 +1000

    removed: CSS

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,10 +1,3 @@
-import Image, { type ImageProps } from "next/image";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
 
 export default function Home() {
   return (
