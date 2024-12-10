487755823831f3e04a1ee7f57f2bdd5b4d6db5b0
Multinite <founders.multinite@gmail.com>
2024-12-10 17:36:34 +1000

    add: admin plugin

--- apps/demo/lib/auth.ts
+++ apps/demo/lib/auth.ts
@@ -2,11 +2,12 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "../db";
 import { dashboardPluginWrapper } from "better-auth-dashboard";
+import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
   appName: "admin-dashboard-demo",
-  plugins: dashboardPluginWrapper([], {}),
+  plugins: dashboardPluginWrapper([admin()], {}),
 });
